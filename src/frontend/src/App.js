17d42d0fe4db530f0f802d360a1723dc993f4b85
Raghav Sikka <ricksikka1@gmail.com>
2020-07-26 15:17:26 -0400

    added nginx configuration

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import BasicForm from "./components/BasicForm.js";
 
 function App() {
-  return <BasicForm></BasicForm>;
+ return <BasicForm></BasicForm>;
 }
 
 export default App;
